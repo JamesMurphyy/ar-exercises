46aadf2c89d855c1ef83b68b12e4f12c3f21d439
James Murphy <jamesm8228@gmail.com>
2022-04-04 19:57:10 -0400

    Add exersise 5 answers

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+
+@sum_of_stores = Store.sum(:annual_revenue)
+
+@revenue_of_stores = Store.average(:annual_revenue)
+
+@stores_with_a_million = Store.where("annual_revenue > 1000000").count
+
+puts @sum_of_stores
+puts @revenue_of_stores
+puts @stores_with_a_million
