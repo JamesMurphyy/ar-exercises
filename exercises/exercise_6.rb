76e4d522039b9f0552b41f23b21c507696789ace
James Murphy <jamesm8228@gmail.com>
2022-04-04 20:09:26 -0400

    Refactor and add answer for exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jack", last_name: "Black", hourly_rate: 1000)
+@store1.employees.create(first_name: "Brad", last_name: "Pitt", hourly_rate: 78)
+
+
+
+@store2.employees.create(first_name: "Chris", last_name: "Tucker", hourly_rate: 412)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 45)
+@store2.employees.create(first_name: "Snow", last_name: "White", hourly_rate: 34)
